9a48eddd353cab5ed2878ba65c679c365445c411
Al-Amin <alamincse12@gmail.com>
2024-06-30 14:52:34 +0600

    test block as per run is added

---
+++ cypress/e2e/test_case_spec.cy.js
@@ -0,0 +1,17 @@
+import HomeLocator from "../pages/HomePage";
+const home_obj=new HomeLocator();
+describe('Test Case 1: Register User', () => {
+    before("Navigate to url http://automationexercise.com ",() => {
+        cy.visit("https://automationexercise.com/");
+    });
+    it('Verify that home page is visible successfully', () => {
+        home_obj.home_page_logo().should('exist');
+    });
+    it('Click on Signup/Login button', () => {
+        home_obj.signup_login_button().click();
+    });
+    it('Verify New User Signup! is visible', () => {
+        home_obj.new_user_signup_text().should('have.text',"New User Signup!");
+    });
+
+});
